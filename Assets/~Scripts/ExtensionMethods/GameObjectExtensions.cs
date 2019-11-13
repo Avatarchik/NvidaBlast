01ec292142155ab2310e58d0d03e921765f6cafe
Cathal McCosker <cathal.mccosker@gmail.com>
2019-11-14 00:51:34 +0900

    AutoCommit

--- Assets/~Scripts/ExtensionMethods/GameObjectExtensions.cs
+++ Assets/~Scripts/ExtensionMethods/GameObjectExtensions.cs
@@ -1,9 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public static class GameObjectExtensions
 {
+	public static void ApplyToChildren(this GameObject gameObject, Action<GameObject> action)
+	{
+		if (gameObject == null) return;
+		foreach (var trans in gameObject.GetComponentsInChildren<Transform>(true))
+		{
+			action(trans.gameObject);
+		}
+	}
+
 	public static T[] GetComponentsInChildrenWithTag<T>(this GameObject gameObject, string tag)
 			where T : Component
 	{
