05785606ba945fc8cff737a8871fdb0ad61387da
Cathal McCosker <cathal.mccosker@gmail.com>
2019-11-13 23:11:54 +0900

    AutoCommit

--- Assets/ThirdParty/NvBlast/Scripts/Destructible.cs
+++ Assets/ThirdParty/NvBlast/Scripts/Destructible.cs
@@ -8,8 +8,12 @@ public class Destructible : MonoBehaviour
 
 	public void EnableDestructablePhysics()
 	{
+		// In this caes the fractured mesh isn't a good use of the object pool. We just switch the mesh out one time and we
+		// don't contantly create and destroy it
 		var fracturedMeshInstance = Instantiate(fracturedPrefab, transform.position, transform.rotation);
+		// Set the parent to the destructableMeshHolder to keep the Hierarchy neat
 		fracturedMeshInstance.transform.parent = DestructionUpdateManager.Singleton.destructableMeshHolder;
+		// Remove the original non-fractured mesh
 		Destroy(transform.gameObject);
 	}
 }
